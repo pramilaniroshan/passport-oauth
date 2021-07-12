7c69bd23ce3b2c3871856ad466e8f782e22584fb
pramilaniroshan <pramilaniroshan@gmail.com>
2021-07-12 09:42:13 +0530

    passport js and google st added

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
-
+const authRoutes = require('./routes/auth-routes')
 const app = express();
 
 // set up view engine
 
 app.set('view engine','ejs');
+
+//setup routes
+
+app.use('/auth',authRoutes);
 
 app.get('/',(req,res)=>{
    res.render('home'); 
