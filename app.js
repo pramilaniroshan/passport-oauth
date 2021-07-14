754bacabc4cb6d5ea2d413b806764d5f9833112d
pramilaniroshan <pramilaniroshan@gmail.com>
2021-07-14 12:54:39 +0530

    auth checked and profile page done

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
+const profileRoutes = require('./routes/profile-routes');
 const passportSetup = require('./config/passport-setup');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const { initialize } = require('passport');
+const passport = require('passport');
 
+
+
+//setup express
 const app = express();
 
 // set up view engine
 
 app.set('view engine','ejs');
+
+app.use(cookieSession({
+    maxAge : 24 * 60 * 60 * 1000,
+    keys : [keys.session.cookieKey]
+}));
+
+//initialize passport with cookies
+app.use(passport.initialize());
+app.use(passport.session()); 
 
 //conect to mongodb
 mongoose.connect(keys.mongodb.dbURI, {
@@ -20,6 +36,7 @@ mongoose.connect(keys.mongodb.dbURI, {
 //setup routes
 
 app.use('/auth',authRoutes);
+app.use('/profile',profileRoutes)
 
 app.get('/',(req,res)=>{
    res.render('home'); 
